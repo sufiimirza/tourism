a6ba28c8ad5baf4b5f1fad129439bd4addaf891c
sufiimirza <sufiimirza@gmail.com>
2021-09-05 22:13:12 +0800

    Update cityedit.php

--- admin/technichian/cityedit.php
+++ admin/technichian/cityedit.php
@@ -156,7 +156,7 @@ $query  = "select * from admin where username='$aname'";
 							<div class="form-group">
 								<label for="postcode" class="col-sm-3 control-label">Postcode</label>
 								<div class="col-sm-9">
-									<input type="text" class="form-control" name="postcode"  value="<?php echo $row['postcode'] ?>">
+									<input type="text" class="form-control" name="postcode" pattern="[0-9]{5}" value="<?php echo $row['postcode'] ?>">
 								</div>
 							</div>
 							
